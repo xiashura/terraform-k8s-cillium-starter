f7b62fa7e62006bbf812898c5f120d17a4af4d1e
xiashura <plexanov@protonmail.com>
2022-10-12 03:54:33 +0300

    init dev shell

--- flake.nix
+++ flake.nix
@@ -1,11 +1,32 @@
 {
-  description = "A very basic flake";
-  
-  outputs = { self, nixpkgs }: {
 
-    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
-
-    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
-
+  description = "terraform-k8s-cillium-starter";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable"; 
+    flake-utils.url  = "github:numtide/flake-utils";
   };
+
+  outputs = { self, nixpkgs, flake-utils }: flake-utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = import nixpkgs { inherit system; };
+
+    in with pkgs; {
+      devShell = mkShell {
+        buildInputs = [
+          cloud-utils
+          packer
+          cdrkit
+          cfssl
+          jq
+          openssl
+          terraform
+          terraform-providers.libvirt
+          kubectl
+          kubernetes-helm-wrapped
+          cilium-cli
+          hubble
+        ];
+      };
+    }
+  );
 }
