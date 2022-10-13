19145289d0883ba8907215c98992e8ea5f22e93c
xiashura <plexanov@protonmail.com>
2022-10-13 13:23:18 +0300

    add packer build default k8s-image

---
+++ cloud-init/scripts/install-k8s.sh
@@ -0,0 +1,69 @@
+#!/bin/bash -eux
+
+apt update -y  
+apt install -y apt-transport-https ca-certificates \
+  curl lsb-core dpkg \
+  software-properties-common \
+  gnupg2 
+
+curl -fsSL "https://download.docker.com/linux/ubuntu/gpg" | sudo gpg --dearmor --yes -o /usr/share/keyrings/docker-archive-keyring.gpg
+
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" >> /etc/apt/sources.list.d/docker.list 
+
+apt update -y && apt install -y \
+  containerd.io \
+  docker-ce \
+  docker-ce-cli
+
+docker info
+
+containerd config default > /etc/containerd/config.toml 
+sed -i 's/SystemdCgroup \= false/SystemdCgroup \= true/g' /etc/containerd/config.toml
+
+cat > /etc/modules-load.d/containerd.conf <<EOF
+overlay
+br_netfilter
+EOF
+
+modprobe overlay
+modprobe br_netfilter
+
+
+cat > /etc/docker/daemon.json <<EOF
+{
+  "exec-opts": ["native.cgroupdriver=systemd"],
+  "log-driver": "json-file",
+  "log-opts": {
+    "max-size": "100m"
+  },
+  "storage-driver": "overlay2"
+}
+EOF
+
+mkdir -p /etc/systemd/system/docker.service.d
+
+systemctl daemon-reload
+systemctl restart docker
+
+systemctl restart containerd
+
+curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
+echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" >> /etc/apt/sources.list.d/kubernetes.list
+
+sudo apt update -y 
+sudo apt install -y kubelet kubeadm kubectl
+sudo apt-mark hold kubelet kubeadm kubectl
+
+cat > /etc/sysctl.d/kubernetes.conf <<EOF
+net.bridge.bridge-nf-call-ip6tables=1
+net.bridge.bridge-nf-call-iptables=1
+net.ipv4.ip_forward=1
+EOF
+
+systemctl daemon-reload
+systemctl restart kubelet
+
+sed -i -e 's/#DNS=/DNS=8.8.8.8/g' /etc/systemd/resolved.conf 
+systemctl restart systemd-resolved
+
+swapoff -a
--- flake.nix
+++ flake.nix
@@ -10,10 +10,19 @@
     let
       pkgs = import nixpkgs { inherit system; };
 
+      cloud-init-build-img = pkgs.writeShellScriptBin "cloud-init-build-img" ''
+        mkdir -p $PWD/tmp
+        rm -rf ./tmp/cloud-init.img
+        sed -i "s|SSH_KEY|$(cat ~/.ssh/id_rsa.pub)|g" cloud-init/packer.yml
+        cloud-localds ./tmp/cloud-init.img cloud-init/packer.yml 
+        sed -i "s|$(cat ~/.ssh/id_rsa.pub)|SSH_KEY|g" cloud-init/packer.yml
+      '';
+
     in with pkgs; {
       devShell = mkShell {
         buildInputs = [
           cloud-utils
+          cloud-init-build-img
           packer
           cdrkit
           cfssl
