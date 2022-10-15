27867ff3985cd0921bfda351a6c6129ae8f30c84
xiashura <plexanov@protonmail.com>
2022-10-16 02:34:26 +0300

    create module kubeadm

---
+++ modules/k8s-kubeadm/main.tf
@@ -0,0 +1,58 @@
+
+resource "null_resource" "init-master-node" {
+
+  connection {
+    type        = "ssh"
+    user        = var.master-node.user
+    private_key = file(var.ssh-key-private-path)
+    host        = var.master-node.ip
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "kubeadm init",
+      "mkdir -p $HOME/.kube",
+      "sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config",
+      "sudo chown $(id -u):$(id -g) $HOME/.kube/config",
+    ]
+  }
+
+  provisioner "local-exec" {
+    command = <<EOF
+      TOKEN=$(ssh -o StrictHostKeyChecking=no ${var.master-node.user}@${var.master-node.ip} sudo kubeadm token list | tail -1 | cut -f 1 -d " ") 
+      HASH=$(ssh -o StrictHostKeyChecking=no ${var.master-node.user}@${var.master-node.ip} openssl x509 -pubkey -in /etc/kubernetes/pki/ca.crt | openssl rsa -pubin -outform der 2>/dev/null | openssl dgst -sha256 -hex | sed 's/^.* //' ) 
+      echo "kubeadm join ${var.master-node.ip}:6443 --token $TOKEN --discovery-token-ca-cert-hash sha256:$HASH" > ${path.root}/tmp/script-worker-join-${var.name-cluster}.sh
+    EOF
+  }
+
+}
+
+
+resource "null_resource" "join-worker-nodes" {
+
+  depends_on = [
+    null_resource.init-master-node
+  ]
+
+  count = length(var.worker-nodes)
+
+  connection {
+    type        = "ssh"
+    user        = var.worker-nodes[count.index].user
+    private_key = file(var.ssh-key-private-path)
+    host        = var.worker-nodes[count.index].ip
+  }
+
+  provisioner "file" {
+    source      = "${path.root}/tmp/script-worker-join-${var.name-cluster}.sh"
+    destination = "/root/script-worker-join-${var.name-cluster}.sh"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "bash /root/script-worker-join-${var.name-cluster}.sh"
+    ]
+  }
+
+
+}
---
+++ modules/k8s-kubeadm/variables.tf
@@ -0,0 +1,24 @@
+
+variable "name-cluster" {
+  default = "cluster-n"
+}
+
+variable "ssh-key-private-path" {
+  default = "~/.ssh/id_rsa"
+}
+
+variable "master-node" {
+  default = {
+    user = "root",
+    ip   = "192.186.0.1",
+  }
+}
+
+
+variable "worker-nodes" {
+  type = list(object({
+    user = string,
+    ip   = string,
+    })
+  )
+}
