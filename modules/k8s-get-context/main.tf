796366c1cb70c82b7d192b2f8163bb81c367cc5c
xiashura <plexanov@protonmail.com>
2022-10-19 18:45:24 +0300

    create module k8s get context

---
+++ modules/k8s-get-context/main.tf
@@ -0,0 +1,19 @@
+
+resource "null_resource" "download-k8s-context" {
+
+
+  connection {
+    type        = "ssh"
+    user        = var.ssh-user-master-node
+    private_key = file(var.ssh-key-private-path)
+    host        = var.ssh-host-master-node
+  }
+
+  provisioner "local-exec" {
+    command = <<EOF
+      mkdir -p ${var.context-path}/context && \
+      rsync -e "ssh -o StrictHostKeyChecking=no" -Wav --progress ${var.ssh-user-master-node}@${var.ssh-host-master-node}:/etc/kubernetes/admin.conf ${var.context-path}/context/${var.cluster-name}
+    EOF
+  }
+
+}
---
+++ modules/k8s-get-context/variables.tf
@@ -0,0 +1,23 @@
+variable "cluster-name" {
+  default = "cluster-context-n"
+}
+
+variable "ssh-key-private-path" {
+  default = "~/.ssh/id_rsa"
+}
+
+variable "ssh-user-master-node" {
+  default = "root"
+}
+
+
+variable "ssh-host-master-node" {
+  default = "192.168.0.101"
+}
+
+variable "context-path" {
+
+}
+
+
+variable "id_kubeadm_dependsi" {}
