68e3ac82f228821f82de59b0231e28c9351d98c3
xiashura <plexanov@protonmail.com>
2022-10-21 04:37:54 +0300

    output change namespace and add

--- modules/k8s-get-context/outputs.tf
+++ modules/k8s-get-context/outputs.tf
@@ -1,4 +1,8 @@
 
-output "name" {
+output "id_dependsi" {
   value = null_resource.download-k8s-context.triggers.id_dependsi
+}
+
+output "path" {
+  value = "${var.context-path}/context/${var.cluster-name}.yml"
 }
